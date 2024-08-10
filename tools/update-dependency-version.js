911bc5cbd8b639e30ec0ee040d53d412e2646bcb
veera <marni.srikanth@gmail.com>
2024-08-10 18:58:14 -0400

    update

--- tools/update-dependency-version.js
+++ tools/update-dependency-version.js
@@ -11,96 +11,116 @@
 /* eslint-disable no-console */
 /* eslint-disable @typescript-eslint/restrict-template-expressions */
 /* eslint-disable consistent-return */
+
 const glob = require('glob');
 const path = require('path');
 const fs = require('fs');
-
-const SERVER_FOLDER = './servers';
 const simpleGit = require('simple-git');
+const { execSync } = require('child_process');
 
 const git = simpleGit();
+const monorepoRoot = path.resolve(__dirname, '..');
 
-const searchAndUpdate = (dependencies, filePath, obj) => {
-    const fileWrie = filePath;
+const JSON_SPACING = 4;
+const findPackageJsonFiles = () => {
+    return new Promise((resolve, reject) => {
+        glob(
+            `${monorepoRoot}/+(servers|portable-devices|packages|packages-modules)/**/package.json`,
+            { onlyFiles: false, ignore: '**/node_modules/**' },
+            (err, files) => {
+                if (err) reject(`Unable to scan directory: ${err}`);
+                resolve(files);
+            },
+        );
+    });
+};
+
+const buildPackageMap = async () => {
+    const packageJsonFiles = await findPackageJsonFiles();
+    const packageMap = new Map();
+
+    packageJsonFiles.forEach((file) => {
+        const packageJson = JSON.parse(fs.readFileSync(file, 'utf8'));
+        if (packageJson.name) {
+            packageMap.set(packageJson.name, {
+                path: path.dirname(file),
+                version: packageJson.version,
+            });
+        }
+    });
+
+    return packageMap;
+};
+
+const searchAndUpdate = (dependencies, filePath, obj, packageMap) => {
     const packageDir = path.dirname(filePath);
-    console.log('---PACKAGE DIR', packageDir);
-    for (const key in dependencies) {
-        if (dependencies[key].includes('link:')) {
-            const relativeDepFolder = dependencies[key].split('link:')[1];
-            console.log('--FOLDER ROAD', relativeDepFolder);
-            const dependencyFolder = path.join(packageDir, relativeDepFolder);
-            try {
-                fs.readdirSync(dependencyFolder);
-            } catch (err) {
 
-                console.log(`--- err ${err.message}`);
-                console.log(
-                    `err Search for dependency of ${filePath} with package path ${relativeDepFolder} not found`,
-                );
+    for (const key in dependencies) {
+        if (dependencies[key].startsWith('link:')) {
+            const relativeDepFolder = dependencies[key].split('link:')[1];
+            const dependencyFolder = path.join(packageDir, relativeDepFolder);
+
+            try {
+                const packageJsonPath = path.join(dependencyFolder, 'package.json');
+                const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf8'));
+                dependencies[key] = packageJson.version;
+                const str = JSON.stringify(obj, null, JSON_SPACING);
+                fs.writeFileSync(filePath, str, 'utf8');
+            } catch (err) {
+                console.error(`Error updating ${key} in ${filePath}: ${err.message}`);
                 throw err;
             }
-            glob(`${dependencyFolder}/package.json`, { ignore: '**/node_modules/**' }, (err, files) => {
-                if (err) return console.error(`Unable to scan directory: ${err}`);
-                console.log(files);
-                files.forEach((file) => {
-                    fs.readFile(file, 'utf-8', (err, data) => {
-                        if (err) return console.error(`Unable to scan directory: ${err}`);
-                        try {
-                            const objVersion = JSON.parse(data);
-                            const { version } = objVersion;
-                            dependencies[key] = `${version}`;
-                            const str = JSON.stringify(obj, null, 2);
-                            fs.writeFileSync(fileWrie, str, 'ascii');
-                        } catch (err) {
-                            console.error(`Failed at file: ${file}`)
-                            throw (err);
-                        }
-
-                    });
-                });
-            });
+        } else if (packageMap.has(key)) {
+            dependencies[key] = packageMap.get(key).version;
+            const str = JSON.stringify(obj, null, JSON_SPACING);
+            fs.writeFileSync(filePath, str, 'utf8');
         }
     }
 };
 
-glob(
-    './+(servers|portable-devices|packages|packages-modules)/**/package.json',
-    { onlyFiles: false, ignore: '**/node_modules/**' },
-    (err, files) => {
-        if (err) return console.error(`Unable to scan directory: ${err}`);
-        files.forEach((file) => {
-            if (!file.includes('node_modules')) {
-                fs.readFile(file, 'utf-8', (err, data) => {
-                    if (err) return console.error(`Unable to scan directory: ${err}`);
-                    try {
-                        const obj = JSON.parse(data);
-                        const { dependencies, peerDependencies, devDependencies } = obj;
-                        searchAndUpdate(dependencies, file, obj);
-                        searchAndUpdate(peerDependencies, file, obj);
-                        searchAndUpdate(devDependencies, file, obj);
-                    } catch (err) {
-                        console.error(`Errored at ${file}`);
-                        console.error(err);
-                    }
-                });
-            }
-        });
-        git.add('.')
-            .then(() => {
-                git.status()
-                    .then((status) => {
-                        console.log('POST GIT CHANGES', status);
-                        if (status.modified.length) {
-                            const fileArray = status.modified.filter((element) => element.includes('package.json'));
-                            const addArray = fileArray.map((element) => `./${element}`);
-                            git.add(addArray);
-                            git.commit('corrected packages version!');
-                        } else console.log('no change');
-                    })
-                    .catch((err) => {
-                        console.error(err);
-                    });
-            })
-            .catch((err) => console.error(err));
-    },
-);
+const updateDependencies = async () => {
+    const packageMap = await buildPackageMap();
+    const packageJsonFiles = await findPackageJsonFiles();
+
+    packageJsonFiles.forEach((file) => {
+        if (!file.includes('node_modules')) {
+            fs.readFile(file, 'utf-8', (err, data) => {
+                if (err) return console.error(`Unable to read file: ${err}`);
+                try {
+                    const obj = JSON.parse(data);
+                    const { dependencies, peerDependencies, devDependencies } = obj;
+                    searchAndUpdate(dependencies, file, obj, packageMap);
+                    searchAndUpdate(peerDependencies, file, obj, packageMap);
+                    searchAndUpdate(devDependencies, file, obj, packageMap);
+                } catch (err) {
+                    console.error(`Error processing ${file}: ${err.message}`);
+                }
+            });
+        }
+    });
+
+    try {
+        execSync('npx prettier --write "**/package.json"', { stdio: 'inherit' });
+    } catch (err) {
+        console.error(`Error running prettier: ${err.message}`);
+    }
+
+    await git.add('.');
+    const status = await git.status();
+    console.log('POST GIT CHANGES', status);
+
+    if (status.modified.length) {
+        const fileArray = status.modified.filter((element) => element.includes('package.json'));
+        const addArray = fileArray.map((element) => `./${element}`);
+        await git.add(addArray);
+        await git.commit('Updated packages to use correct versions and linked dependencies');
+    } else {
+        console.log('No changes detected');
+    }
+};
+
+updateDependencies()
+    .then(() => {
+        console.log('Dependencies updated done!');
+    })
+    .catch((err) => console.error(`Error in updateDependencies: ${err.message}`));
