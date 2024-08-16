3c62ea6916de1b991db5f2a8ad1722f2bbf483ff
veera <marni.srikanth@gmail.com>
2024-08-16 06:37:19 -0400

    update

--- tools/sortPackageJson.mjs
+++ tools/sortPackageJson.mjs
@@ -11,24 +11,28 @@ const sortPackageJson = await import('sort-package-json').then(module => module.
 
 // Directories to process
 const directories = [
-  path.join(__dirname, '../packages'),
+  path.join(__dirname, '../'),
   path.join(__dirname, '../packages-modules'),
+  path.join(__dirname, '../packages'),
   path.join(__dirname, '../servers'),
   path.join(__dirname, '../portable-devices')
 ];
 
+// Function to sort a single package.json file
 async function sortPackageJsonFile(filePath) {
   const packageJson = JSON.parse(await fsPromises.readFile(filePath, 'utf8'));
   const sortedPackageJson = sortPackageJson(packageJson);
   await fsPromises.writeFile(filePath, JSON.stringify(sortedPackageJson, null, 2) + '\n');
 }
 
+// Recursive function to sort all package.json files in a directory, skipping node_modules
 async function sortAllPackageJsonFiles(dir) {
   const files = await fsPromises.readdir(dir);
   for (const file of files) {
     const fullPath = path.join(dir, file);
     const stat = await fsPromises.lstat(fullPath);
-    if (stat.isDirectory()) {
+    
+    if (stat.isDirectory() && file !== 'node_modules') {
       await sortAllPackageJsonFiles(fullPath);
     } else if (file === 'package.json') {
       await sortPackageJsonFile(fullPath);
@@ -37,10 +41,12 @@ async function sortAllPackageJsonFiles(dir) {
   }
 }
 
+// Function to process all directories
 async function processDirectories(dirs) {
   for (const dir of dirs) {
     await sortAllPackageJsonFiles(dir);
   }
 }
 
+// Execute the sorting process
 await processDirectories(directories);
