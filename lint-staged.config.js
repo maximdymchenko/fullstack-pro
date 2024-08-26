d1eab05535219f3c218e137aa7831fa0c2b62354
veera <marni.srikanth@gmail.com>
2024-08-26 13:00:27 -0400

    update

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -2,6 +2,4 @@ module.exports = {
     '*.{js,jsx,ts,tsx,json,md}': ['prettier --write', 'git add'],
     // '*.{ts,tsx}': ['eslint --fix'], // this can be tested
     'package.json': ['sort-package-json', 'prettier --write'],
-    '*/**/package.json': ['sort-package-json', 'prettier --write'],
-    '**/**/*/package.json': ['sort-package-json', 'prettier --write'],
 };
